e29c9b6d53be82f431ba69ec4f49807ff394813c
Ayaz Khan <iamlordayaz@gmail.com>
2024-05-11 14:26:55 +0530

    Tested working fine now have to intergrated with backend

--- frontend/src/app/components/board/board.component.ts
+++ frontend/src/app/components/board/board.component.ts
@@ -1,15 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Chess } from 'chess.js';
+import { Chess, Square } from 'chess.js';
 
 @Component({
   selector: 'app-board',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.css'
+  styleUrl: './board.component.css',
 })
 export class BoardComponent {
   chess = new Chess();
-
+  clicked: Square | null;
+  constructor() {
+    this.clicked = null;
+  }
+  handleClick(row: any, col: any) {
+    const squareRep = (String.fromCharCode(97 + (col % 8)) +
+      '' +
+      (8 - row)) as Square;
+    if (!this.clicked) {
+      this.clicked = squareRep;
+      return;
+    }
+    this.chess.move({ from: this.clicked, to: squareRep });
+    this.clicked = null;
+  }
 }
